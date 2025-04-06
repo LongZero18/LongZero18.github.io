0bd15f91b668fa2fe44d2ca3d0a758cabaffdf7a
TecJS <jaiseba_2011@hotmail.com>
2025-04-06 13:38:47 -0600

    referecniado correctamente

--- projects.js
+++ projects.js
@@ -3,12 +3,12 @@ const projects = [
         title: "Creador de Personajes",
         description: "Primero usa este",
         imageUrl: "img/Phobe1.jpg",
-        link: "Proyectos/Carrusel/Carrusel.html"
+        link: "creadorpj/index.html"
     },
     {
         title: "Inventario",
         description: "Luego este una vez sepas tu oro y Fuerza que es cuanto puedes cargar",
         imageUrl: "img/Phobe2.png",
-        link: "Proyectos/CiclosFor/Ciclos_For.html"
+        link: "Inventario/index.html"
     }
 ];
