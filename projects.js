c197bd224c69097d1ff8d302a7ffcd6bb9d0f78a
LongZero18 <104640128+LongZero18@users.noreply.github.com>
2025-04-06 14:33:38 -0500

    Mejora
    Se andio menu, y creador de perosanje, un dado sin referenciar

---
+++ projects.js
@@ -0,0 +1,14 @@
+const projects = [
+    {
+        title: "Creador de Personajes",
+        description: "Primero usa este",
+        imageUrl: "img/Phobe1.jpg",
+        link: "Proyectos/Carrusel/Carrusel.html"
+    },
+    {
+        title: "Inventario",
+        description: "Luego este una vez sepas tu oro y Fuerza que es cuanto puedes cargar",
+        imageUrl: "img/Phobe2.png",
+        link: "Proyectos/CiclosFor/Ciclos_For.html"
+    }
+];
---
+++ style.cs
@@ -0,0 +1,9 @@
+.project-card {
+    transition: transform 0.3s ease-in-out;
+}
+
+.project-card:hover {
+    transform: scale(1.05);
+}
+
+/* Puedes agregar más estilos personalizados aquí */
